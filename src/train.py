343b9648f4cb6264c11d137c5907a0a7fc2a611c
Roger Galindo <rogherschrodinger@gmail.com>
2024-05-23 20:37:39 -0500

    run train pipeline

--- src/train.py
+++ src/train.py
@@ -1,22 +1,31 @@
 import os
 import hydra
-import json
 
-from omegaconf import DictConfig
+
+from omegaconf import DictConfig, OmegaConf
+from pathlib import Path
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import LatentDirichletAllocation 
+
+
+DIR_PATH = Path(__file__).resolve().parents[1]
+
 
 @hydra.main(config_path="../config", config_name="main.yaml", version_base=None)
 def main(config: DictConfig):
     
     # load data
-    filename = os.path.join(config.data.processed.path, 'data.json')
-    dict_data = json.load(open(filename))
+    filename = os.path.join(DIR_PATH, config.data.processed.path, 'data.json')
+    print(f"Load data from {filename}")
+
+    # use omegaconf to load yaml file
+    dict_data = OmegaConf.load(filename)
+
 
     vectorizer, tfidf_matrix = vectorization(dict_data)
 
-    n_topics = 10
+    n_topics = 20
     lda = LatentDirichletAllocation(n_components=n_topics, random_state=42)
     lda.fit(tfidf_matrix)
 
