3daa96eb7eb420ed4880855de7e3c975a354b020
Roger Galindo <rogherschrodinger@gmail.com>
2024-05-26 13:51:15 -0500

    load json file

--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -9,7 +9,18 @@ DIR_PATH = Path(__file__).resolve().parents[1]
 
 @hydra.main(config_path="../config", config_name="main.yaml", version_base=None)
 def main(config: DictConfig):
-    pass
+    dict_data = load_data(config)
+
+
+def load_data(config: DictConfig) -> dict:
+    logger = logging.getLogger('load_data')
+
+    filename = os.path.join(DIR_PATH, config.data.processed.path, 'data.json')
+    logger.info(f"Load data from {filename}")
+
+    # use omegaconf to load file
+    dict_data = OmegaConf.load(filename)
+    return dict_data
 
 if __name__ == '__main__':
     main()
